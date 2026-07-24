3b654b71d87c1cd4e9f425c8d0b5d4f9f3ae44fc
Mendes Capuqui <mendescapuqui@gmail.com>
2026-07-24 17:11:55 +0100

    36 - Entrega de paramentros de roteamento por API

--- app.js
+++ app.js
@@ -6,13 +6,50 @@ const fs = require('fs');
 let app = express();
 let movies = JSON.parse(fs.readFileSync('./data/movies.js'));
 
+//GET-/api/v1/movies
 app.get('/api/v1/movies', (req, res)=>{
     res.status(200).json({
         status: "sucess",
-        count: movies.lenght,
+        count: movies.length,
         data:{
             movies: movies
         }
+    })
+})
+//GET-/api/v1/movies/id
+app.get('/api/v1/movies/:id', (req, res)=>{
+    const id = req.params.id * 1;
+    const movie = movies.find(el => el.id === id);
+
+    if (movie === undefined){
+        return res.status(404).json({
+            status: "fail",
+            message: "Movie with ID " + id + " not found"
+        })
+    }
+    res.status(200).json({
+        status: "success",
+        data:{
+            movie: movie
+        }
+    })
+});
+
+//POST - api/v1/movies
+app.use(express.json());
+app.post('/api/v1/movies', (req, res)=>{
+    console.log(req.body);
+
+    const newId = movies[movies.length - 1].id + 1;
+    const newMovie = Object.assign({id: newId}, req.body);
+    movies.push(newMovie);
+    fs.writeFile('./data/movies.js', JSON.stringify(movies), err=>{
+        res.status(201).json({
+            status: "success",
+            data:{
+                movie: newMovie
+            }
+        })
     })
 })
 
