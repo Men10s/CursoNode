1eccd40ffc2eca440e99b7d3a3ba8001c2fd65a8
rmucuambi-maker <rmucuambi@gmail.com>
2026-08-26 12:05:26 +0100

    49-Adicao do mongoose e conexao com MongoDB

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 dotenv.config({path: './config.env'});
 
 const app = require('./app');
 
-console.log(process.env);
-
+//console.log(process.env);
+mongoose.connect(process.env.CONN_STR).then(()=>{
+    console.log('DB connection Sucessfull')
+}).catch((error)=>{
+    console.error('DB connection failed:', error.message);
+});
 
 //Create a Server
 const port = process.env.PORT || 3000;
 let message = "Server is started";
-
 app.listen(port, ()=>{
     console.log(message);
 })
