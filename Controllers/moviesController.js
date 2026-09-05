ed74fa46025148576486183f500c7bcf9307b808
rmucuambi-maker <rmucuambi@gmail.com>
2026-09-05 22:19:11 +0100

    79_Pagination___Using_MongoDB_with_Express

--- Controllers/moviesController.js
+++ Controllers/moviesController.js
@@ -40,8 +40,17 @@ exports.getAllMovies = async (req, res) => {
         }else{
             query = query.select('-__v');
         }
+
+        const page = req.query.page * 1 || 1;
+        const limit = req.query.limit * 1 || 10;
+        const skip = (page - 1) * limit;
+        query = query.skip(skip).limit(limit);
         const movies = await query;
         
+        if(req.query.page) {
+            const numMovies = await Movie.countDocuments();
+            if(skip >= numMovies) throw new Error('This page does not exist');
+        }
         res.status(200).json({
             status: 'success',
             length: movies.length, 
